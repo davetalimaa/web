7744d77a0d1607620bc771aa214ae1b152958316
dave.talimaa <dave.talimaa@khk.ee>
2017-03-15 15:32:41 +0200

    Koostatud rakenduse konfiguratsiooni esmane versioon

---
+++ conf.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dave.talimaa
+ * Date: 15.03.2017
+ * Time: 15:25
+ */
+//defineerime vajalikud konstandid
+define('CLASSES_DIR', 'classes/'); // classes kataloogi nime konstant
+define('TMPL_DIR', 'tmp/'); //tmpl kataloogi nime konstant
+?>
--- index.php
+++ index.php
@@ -5,5 +5,8 @@
  * Date: 15.03.2017
  * Time: 13:08
  */
+// võtame konfiguratsiooni kasutusele
+require_once 'conf.php';
+// pealehe sisu
 echo '<h1>Veebiprogrammeerimine esileht</h1>'
 ?>
