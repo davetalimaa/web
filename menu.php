37fe1eeee06313a3023677d4d4d80d6c957b538d
dave.talimaa <dave.talimaa@khk.ee>
2017-03-22 15:30:04 +0200

    konrollime objekti oemasolu ja sisu

---
+++ menu.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dave.talimaa
+ * Date: 22.03.2017
+ * Time: 15:26
+ */
+//loome menüü mallide objektid
+$menu = new template('menu.menu');
+$item = new template('menu.item');
+// konrollime objekti oemasolu ja sisu
+echo '<pre>';
+print_r($menu);
+print_r($item);
+echo '<pre>';
+?>
