c1b2b17d2543be9bab7e780227ccbb425a9bae7b
dave.talimaa <dave.talimaa@khk.ee>
2017-03-22 12:21:06 +0200

    Koostatud template

---
+++ classes/template.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: dave.talimaa
+ * Date: 22.03.2017
+ * Time: 12:11
+ */
+class template
+{// klassi algus
+    //template klassi omadused - muutujad
+    var $file = ''; // html malli faili nimi
+    var $content = false; //html malli faili sisu
+    var_$vars = array(); //html vaade sisu - reaalsed väärtused
+}// klassi lõpp
