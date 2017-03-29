20970aac6092320834e38452dbd470001f43f6be
dave.talimaa <dave.talimaa@khk.ee>
2017-03-29 15:32:18 +0300

    nimetame menüü väljastav element

--- conf.php
+++ conf.php
@@ -23,10 +23,6 @@ require_once CLASSES_DIR.'linkobject.php';
 $http = new linkobject();
 
 // testime linkobjekti tööd
-echo $http->baseUrl;
-echo '<br/>';
-$link = '';
-$http->addToLink($link, 'kasutaja', 'admin');
-echo '<br/>';
-$http->addToLink($link, 'pass', 'qwerty');
+echo $http->baseUrl.'<br />';
+echo $http->getLink(array('kasutaja'=>'admin', 'pass'=>'qwerty'));
 ?>
--- menu.php
+++ menu.php
@@ -9,9 +9,18 @@
 $menu = new template('menu.menu');
 $item = new template('menu.item');
 // lisame sisu
+// nimetame menüü väljastav element
 $item->set('name','esimene');
+//loome antud menüü elemendile lingi
+$link = $http->getLink(array('act'=>'first'));
+//lisame antud link menüüsse
+$item->set('link', $link);
+//lisame valmis link menüü objekti sisse
 $menu->set('items', $item->parse());
+//
 $item->set('name','teine');
+$link = $http->getLink(array('act'=>'second'));
+$item->set('link', $link);
 $menu->add('items', $item->parse());
 // konrollime objekti oemasolu ja sisu
 // kui soovime pidevat asedamist, siis set funktsioon add asemel
